8d64218df9f057df680f9811ac6be0329d7fa8b9
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-06 15:57:40 +0300

    Интерфейс для проверки вхождения пользователя в базе данных и получение пароля пользователя

--- users/interface.py
+++ users/interface.py
@@ -13,3 +13,17 @@ class IUserRepository(ABC):
     def delete_user(self, user_id):
         """Delete user"""
         pass
+
+
+class IUserAuth(ABC):
+    """Interface for user authentication and user credential validation"""
+
+    @abstractmethod
+    def exists_user(self, username, flag):
+        """There is a user in the database"""
+        pass
+
+    @abstractmethod
+    def get_password(self, values):
+        """Getting a password"""
+        pass

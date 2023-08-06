3d9e0bd6e172f8781d61730a91b6636293f7eb90
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-06 16:10:47 +0300

    Создания токена авторизации и получение текущего пользователя

--- users/token_and_current_user.py
+++ users/token_and_current_user.py
@@ -2,16 +2,10 @@ from fastapi import Request, status
 from datetime import timedelta, datetime
 from jose import jwt, JWTError
 from users.settings_for_token import access_token_expire, name_cookies, secret_key, algorithm
-from connect_db import client
+from users.repositories import RedisRepository
 
 ACCESS_TOKEN_EXPIRE_DAY = access_token_expire
 NAME_COOKIES = name_cookies
-
-
-def get_user(username):
-    """Getting a user from the database"""
-    with client as client_redis:
-        return client_redis.hgetall(name=username)
 
 
 def create_access_token(data: dict):
@@ -33,7 +27,7 @@ async def get_current_user(request: Request):
         username = pyload.get("username")  # Получаем из токена имя пользователя
     except JWTError as ex:
         return {"JWT error": f"{ex}"}
-    user = get_user(username)  # Получаем пользователя и базы данных
+    user = RedisRepository.get_user(username)  # Получаем пользователя и базы данных
     if not user:
         return response_unauthorized
     current_user = {

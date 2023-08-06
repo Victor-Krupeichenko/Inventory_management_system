6b275a59772c01b8e3cf6ddab4ece00ccc062e6a
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-06 10:42:49 +0300

    Интерфейс взаимодействия с Redis

--- users/repositories.py
+++ users/repositories.py
@@ -9,17 +9,11 @@ class RedisRepository(IUserRepository):
     def add_user(cls, user):
         """Add user to hash"""
         with client as client_redis:
-            client_redis.hset(user.user_id, mapping=user.user_mapping())
+            client_redis.hset(user.user_hash, mapping=user.user_mapping())
 
     @classmethod
-    def delete_user(cls, user_id):
+    def delete_user(cls, user_hash):
         """Removing a user from hash"""
         with client as client_redis:
-            client_redis.hdel(user_id, user_id)
-            client_redis.delete(user_id)
-
-    @classmethod
-    def generate_user_id(cls):
-        """Genreate id for user"""
-        with client as client_redis:
-            return client_redis.incr("user_id")
+            client_redis.hdel(user_hash, user_hash)
+            client_redis.delete(user_hash)

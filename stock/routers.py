c13dc184a399288915fd07840877ea3e3e89be27
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-08 12:29:38 +0300

    Эндпоинт для расхода материала

--- stock/routers.py
+++ stock/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from stock.schemes import StockProductScheme
 from stock.models import Stock
 from utils import error_checking
@@ -19,6 +19,27 @@ async def create_stock(product: StockProductScheme):
     new_product.add_product()
 
     return {
+        "status": status.HTTP_201_CREATED,
         "product": data.get("product"),
         "quantity": data.get("quantity")
+    }
+
+
+@stock_router.post("/stock_consumption")
+async def stock_consumption(product: StockProductScheme):
+    """Stock consumption"""
+    data = dict(product)
+    errors = await error_checking(data, "error")
+    if errors:
+        return errors
+    consumption_stock = Stock(**data)
+    remainder = consumption_stock.consumption()
+    if not remainder:
+        return {
+            "error": "no such material",
+            "status": status.HTTP_400_BAD_REQUEST
+        }
+    return {
+        "remainder": remainder,
+        "status": status.HTTP_202_ACCEPTED
     }

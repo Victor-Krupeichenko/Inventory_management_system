9ba47195b1a0cef112274f57eff7fcd138e047d3
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-08 12:27:41 +0300

    Интерфейс для взаимодействия с Redis

--- stock/repository.py
+++ stock/repository.py
@@ -14,3 +14,25 @@ class StockRepositoryRedis(UIStock):
                 client_redis.hincrby(name=f"stock:{product.product}", key=f"{product.product}", amount=product.quantity)
             else:
                 client_redis.hset(name=f"stock:{product.product}", key=f"{product.product}", value=product.quantity)
+
+    @classmethod
+    def consumption(cls, product):
+        """Stock consumption"""
+        with client as client_redis:
+            if client_redis.exists(f"stock:{product.product}"):
+                quantity = int(client_redis.hget(name=f"stock:{product.product}", key=f"{product.product}"))
+                if quantity >= product.quantity:
+                    client_redis.hincrby(name=f"stock:{product.product}", key=f"{product.product}",
+                                         amount=-product.quantity)
+                    return {
+                        "remainder": quantity - product.quantity,
+                        "spend": product.quantity
+                    }
+                else:
+                    client_redis.hincrby(name=f"stock:{product.product}", key=f"{product.product}", amount=-quantity)
+                    return {
+                        "remainder": 0,
+                        "spend": quantity
+                    }
+            else:
+                return False

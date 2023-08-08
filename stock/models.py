11878e150a57148965aead382d5c32cbafcaa580
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-08 12:30:26 +0300

    Модель материалов

--- stock/models.py
+++ stock/models.py
@@ -11,3 +11,8 @@ class Stock:
     def add_product(self):
         """Adds a product"""
         StockRepositoryRedis.add_product(self)
+
+    def consumption(self):
+        """Stock consumption"""
+        product_quantity = StockRepositoryRedis.consumption(self)
+        return product_quantity

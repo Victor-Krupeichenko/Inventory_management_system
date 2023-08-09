29919f1850938a556ffdf53f572116def55dd890
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-09 10:49:14 +0300

    Эндпоинты для регистрации пользователя и аутентификация пользователя

---
+++ api/users/routers.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, status, Response
+from api.users.schemes import RegisterUserScheme, AuthUserScheme
+from api.users.models import User
+from api.users.settings_for_token import name_cookies
+from api.users.token_and_current_user import create_access_token
+from api.utils import error_checking
+
+user_router = APIRouter(prefix="/user", tags=["user"])
+
+
+@user_router.post("/register_user")
+async def register_user(user: RegisterUserScheme):
+    """Register User"""
+    data = dict(user)
+    errors = await error_checking(data, "error")
+    if errors:
+        return errors
+    try:
+        new_user = User(username=data.get("username"), password=data.get("password1"), email=data.get("email"))
+        new_user.create_user()
+        response = {
+            "status": status.HTTP_201_CREATED,
+            "username": new_user.username,
+        }
+        return response
+    except Exception as ex:
+        return {"error": f"{ex}", "status": status.HTTP_400_BAD_REQUEST}
+
+
+@user_router.post("/login_user")
+async def login_user(response: Response, user: AuthUserScheme):
+    """Authenticate user"""
+    data = dict(user)
+    errors = await error_checking(data, "error")
+    if errors:
+        return errors
+    jwt_token = create_access_token(data={"username": user.username})
+    response.set_cookie(key=name_cookies, value=f"Bearer {jwt_token}", httponly=True)
+    return {
+        "token": jwt_token,
+        "username": user.username
+    }

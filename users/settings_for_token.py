352608eb8dfd3c4bf3e935eda369d67545716624
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-09 10:26:38 +0300

    Delete users directory

--- users/settings_for_token.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-secret_key = os.getenv("SECRET_KEY_TOKEN")
-algorithm = os.getenv("ALGORITHM_TOKEN")
-name_cookies = os.getenv("NAME_COOKIES")
-access_token_expire = os.getenv("ACCESS_TOKEN_EXPIRE_DAYS")

3e79b5bcfe06e3a08122e5a64bdc4ccb3240d55d
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-09 22:38:20 +0300

    Рендер страницы добавления компании

--- web/routers.py
+++ web/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request, responses, status
 from fastapi.templating import Jinja2Templates
 from api.company.schemes import CompanyCreateScheme
 from web.forms import form_company
-from web.utils import check_error, form_valid_data, object_instance
+from web.web_utils import check_error, form_valid_data, object_instance
 from api.company.models import Company
 
 web_router = APIRouter(include_in_schema=False)
@@ -15,25 +15,32 @@ def home_page(request: Request):
     return templates.TemplateResponse("index.html", context={"request": request})
 
 
-@web_router.get("/add-company")
-def web_add_company(request: Request):
-    """Form Page Render"""
-    form_fields = form_company()
-    return templates.TemplateResponse("create_company.html", context={"request": request, "form_fields": form_fields})
-
-
-@web_router.post("/add-company")
+@web_router.api_route("/add-company", methods=["GET", "POST"])
 async def web_add_company(request: Request):
-    """Sending data from a form"""
-    form_data = await request.form()
-    form = form_valid_data(form_data, "product", CompanyCreateScheme)
-    error_list = check_error(form, "error")
-    if error_list:
-        form_fields = form_company()
+    """Add company page render"""
+    form_fields = form_company()
+    if request.method == "GET":
         return templates.TemplateResponse(
-            name="create_company.html", status_code=status.HTTP_400_BAD_REQUEST,
-            context={"request": request, "form_fields": form_fields, "errors_list": error_list}
+            name="create_company.html", status_code=status.HTTP_200_OK,
+            context={
+                "request": request,
+                "form_fields": form_fields
+            }
         )
-    company = object_instance(form, Company)
-    company.add_company()
-    return responses.RedirectResponse("/", status_code=status.HTTP_302_FOUND)
+    elif request.method == "POST":
+        form_data = await request.form()
+        form_valid = form_valid_data(form_data, "product", CompanyCreateScheme)
+        errors_list = check_error(form_valid, "error")
+        if errors_list:
+            return templates.TemplateResponse(
+                name="create_company.html", status_code=status.HTTP_400_BAD_REQUEST,
+                context={
+                    "request": request,
+                    "form_fields": form_fields,
+                    "errors_list": errors_list
+                }
+            )
+        company = object_instance(form_valid, Company)
+        company.add_company()
+        redirect_url = web_router.url_path_for("home_page")
+        return responses.RedirectResponse(url=redirect_url, status_code=status.HTTP_302_FOUND)

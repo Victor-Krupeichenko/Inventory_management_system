a8f6c8875fa97b5b84ad66f299f1bc6e89f39793
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-10 12:24:04 +0300

    Добавлен эндпоинт для регистрации пользователя

--- web/routers.py
+++ web/routers.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Request, responses, status
 from fastapi.templating import Jinja2Templates
 from api.company.schemes import CompanyCreateScheme
-from web.forms import form_company
-from web.web_utils import check_error, form_valid_data, object_instance
+from web.forms import form_for_models
+from web.web_utils import check_error, form_valid_data, object_instance, templates_error
 from api.company.models import Company
 from api.company.repository import CompanyRepositoryRedis
+from api.users.schemes import RegisterUserScheme
+from api.users.models import User
 
 web_router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="web/templates")
@@ -19,10 +21,11 @@ def home_page(request: Request):
 @web_router.api_route("/add-company", methods=["GET", "POST"])
 async def web_add_company(request: Request):
     """Add company page render"""
-    form_fields = form_company()
+    form_fields = form_for_models(CompanyCreateScheme)
+    name_template = "create_company.html"
     if request.method == "GET":
         return templates.TemplateResponse(
-            name="create_company.html", status_code=status.HTTP_200_OK,
+            name=name_template, status_code=status.HTTP_200_OK,
             context={
                 "request": request,
                 "form_fields": form_fields
@@ -30,16 +33,12 @@ async def web_add_company(request: Request):
         )
     elif request.method == "POST":
         form_data = await request.form()
-        form_valid = form_valid_data(form_data, "product", CompanyCreateScheme)
+        form_valid = form_valid_data(form_data, CompanyCreateScheme, key="product")
         errors_list = check_error(form_valid, "error")
         if errors_list:
-            return templates.TemplateResponse(
-                name="create_company.html", status_code=status.HTTP_400_BAD_REQUEST,
-                context={
-                    "request": request,
-                    "form_fields": form_fields,
-                    "errors_list": errors_list
-                }
+            return templates_error(
+                request, templates=templates, name_template=name_template, status_code=status.HTTP_400_BAD_REQUEST,
+                errors_list=errors_list, form_fields=form_fields
             )
         company = object_instance(form_valid, Company)
         company.add_company()
@@ -56,3 +55,31 @@ async def company_specified_product(request: Request):
         name="index.html",
         context={"request": request, "results": results, "search": search, "count": len(results.get("company"))}
     )
+
+
+@web_router.api_route("/register-user", methods=["GET", "POST"])
+async def web_register_user(request: Request):
+    """Register user"""
+    form_fields = form_for_models(RegisterUserScheme)
+    name_template = "register_user.html"
+    if request.method == "GET":
+        return templates.TemplateResponse(
+            name=name_template, context={
+                "request": request, "form_fields": form_fields
+            }
+        )
+    elif request.method == "POST":
+        form_data = await request.form()
+        form_valid = form_valid_data(form_data, RegisterUserScheme)
+        errors_list = check_error(form_valid, "error")
+        if errors_list:
+            return templates_error(
+                request, templates=templates, name_template=name_template, status_code=status.HTTP_400_BAD_REQUEST,
+                errors_list=errors_list, form_fields=form_fields
+            )
+        new_user = User(
+            username=form_data.get("username"), password=form_data.get("password1"), email=form_data.get("email")
+        )
+        new_user.create_user()
+        redirect_url = web_router.url_path_for("home_page")
+        return responses.RedirectResponse(url=redirect_url, status_code=status.HTTP_302_FOUND)

45131af4a78714e738acacba700edcfc7218dc2f
Victor-Krupeichenko <71899727+Victor-Krupeichenko@users.noreply.github.com>
2023-08-10 13:15:02 +0300

    Форма для моделей

--- web/forms.py
+++ web/forms.py
@@ -1,10 +1,7 @@
-from api.company.schemes import CompanyCreateScheme
-
-
-def form_company():
-    """Form for model Company"""
+def form_for_models(scheme):
+    """Form for model"""
     temp_fields = list()
-    fields = CompanyCreateScheme.model_json_schema().get("properties")
+    fields = scheme.model_json_schema().get("properties")
     for field in fields:
         temp_fields.append({"title": field, "required": "required"})
     return temp_fields
